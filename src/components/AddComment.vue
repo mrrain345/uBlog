30a85f94a239d7fa67409a9e48bcff791b1abc51
szymon-k <szymon.krzywiecki@codetain.co>
2019-05-25 18:54:47 +0200

    Added 'AddComment' component

---
+++ src/components/AddComment.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                <img class="image" src="" alt="Generic placeholder image">
+                </div>
+			    <textarea
+                id="source" 
+                class="form-control col-10"
+                placeholder="Write a comment.." 
+                v-model="content" 
+                ></textarea>
+		    </div>
+            <div class="row" style="margin-top: 8px;">
+                <div class="col-11">
+                </div>
+                <button type="button" class="btn btn-success col-1" @click="comment()">Comment</button>                              
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'AddComment',
+	data() {
+		return {
+			content: ''
+		};
+	},
+	methods: {
+		comment(){
+			console.log(this.content);
+			this.content='';
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+	textarea{
+		min-height: 170px;
+		max-height: 170px;
+	}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 module.exports = {
 	routes: [
 		{ path: '/writer', component: require('./components/MarkdownWriter.vue').default },
-		{ path: '/rest', component: require('./components/RestTest.vue').default }
+		{ path: '/rest', component: require('./components/RestTest.vue').default },
+		{ path: '/comment', component: require('./components/AddComment.vue').default}
 	]
 };
