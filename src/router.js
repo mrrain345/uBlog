12404949924faa9487bb10ed3a3b9af55d491ddc
webnec <lukasz.nec@o2.pl>
2019-05-29 19:13:33 +0200

    add RegistrationForm to router.js

--- src/router.js
+++ src/router.js
@@ -4,5 +4,6 @@ module.exports = {
 		{ path: '/rest', component: require('./components/RestTest.vue').default },
 		{ path: '/add-comment', component: require('./components/AddComment.vue').default},
 		{ path: '/comment', component: require('./components/Comment.vue').default}
+				{ path: '/registration', component: require('./components/RegistrationForm.vue').default}
 	]
 };
