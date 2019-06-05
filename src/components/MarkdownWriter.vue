0322b292bd2f0926cdae90137df74fc8ed3878e7
MrRaiN <mc.rain345@gmail.com>
2019-06-06 00:30:29 +0200

    Changed toolbar in 'MarkdownWriter' component

--- src/components/MarkdownWriter.vue
+++ src/components/MarkdownWriter.vue
@@ -1,12 +1,20 @@
 <template>
 	<div class="container">
 		<div id="toolbar" class="row">
-			<div class="dropdown">
-				Header
+			<div class="btn-group" style="margin-right: 5px;">
+				<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Header</button>
+				<div class="dropdown-menu">
+					<button class="dropdown-item" @click="set_header(1)">Title</button>
+					<button class="dropdown-item" @click="set_header(3)">Subtitle</button>
+					<button class="dropdown-item" @click="set_header(0)">Normal text</button>
+					<div class="dropdown-divider"></div>
+				</div>
 			</div>
-			<div id="b" class="button" @click="insert('**')"><b>B</b></div>
-			<div id="i" class="button" @click="insert('*')"><i>I</i></div>
-			<div id="u" class="button" @click="insert('++')"><u>U</u></div>
+			<div class="btn-group" role="group" aria-label="Basic example">
+				<button type="button" class="btn btn-success" @click="insert('**')">B</button>
+				<button type="button" class="btn btn-success" @click="insert('*')">I</button>
+				<button type="button" class="btn btn-success" @click="insert('++')">U</button>
+			</div>
 		</div>
 		<div class="row">
 			<textarea id="source" class="col-6" v-model="source"></textarea>
@@ -25,23 +33,66 @@ export default {
 	},
 	data() {
 		return {
-			source: '# Markdown\n\nLorem ipsum'
+			source: '# Lorem ipsum\n\n### Lorem ipsum dolor sit amet\nConsectetur adipiscing elit. Curabitur ac ante pretium tortor venenatis fringilla. Quisque mauris augue, sollicitudin quis congue quis, facilisis at nibh. Sed euismod consectetur magna ac malesuada. Morbi erat leo, pulvinar id euismod quis, imperdiet vel nunc. Nulla lacinia malesuada purus, eu maximus leo semper et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vestibulum enim sed vestibulum maximus. Praesent imperdiet in dui ut ultrices. In sit amet aliquet nibh, eu pretium tellus.\n\nSuspendisse rutrum, dolor ultricies luctus tempus, orci diam scelerisque massa, vel posuere neque metus ac nisi. Sed efficitur ut est ac scelerisque. Pellentesque porttitor, ipsum quis semper porta, massa lectus consequat neque, non lobortis ex metus ut diam. Fusce consequat dui sit amet eros ultricies egestas. Praesent quis rhoncus mi, ac tincidunt enim. Sed aliquam luctus urna, et tempor turpis molestie ac. Nunc nec venenatis enim. Nullam elementum metus eu vulputate hendrerit. Pellentesque interdum euismod consequat. Suspendisse consectetur mattis porta. Aenean massa eros, dignissim vitae maximus vel, fringilla a ligula. Morbi felis nibh, viverra maximus nisl vitae, hendrerit bibendum diam. Sed a nibh quam. Nulla commodo tincidunt ultrices. Mauris aliquam orci risus, nec aliquet lacus laoreet eu. Suspendisse pulvinar vehicula lorem tristique fringilla.'
 		};
 	},
 	methods: {
-		insert(str) {
+		set_text(str, selection_start, selection_end) {
 			const textarea = document.getElementById('source');
 			textarea.focus();
 			const start = textarea.selectionStart;
 			const end = textarea.selectionEnd;
-			const txt1 = textarea.value.substr(0, start);
-			const txt2 = textarea.value.substr(start, end-start);
-			const txt3 = textarea.value.substr(end);
 
-			textarea.value = txt1 + str + txt2 + str + txt3;
-			textarea.selectionStart = start + str.length;
-			textarea.selectionEnd = end + str.length;
+			textarea.value = str;
+			textarea.selectionStart = start + selection_start;
+			textarea.selectionEnd = end + selection_end;
 			this.source = textarea.value;
+		},
+
+		insert(str) {
+			const textarea = document.getElementById('source');
+			const start = textarea.selectionStart;
+			const end = textarea.selectionEnd;
+			const value = textarea.value;
+
+			const txt1 = value.substr(0, start-str.length);
+			const txt2 = value.substr(start-str.length, str.length);
+			const txt3 = value.substr(start, end-start);
+			const txt4 = value.substr(end, str.length);
+			const txt5 = value.substr(end+str.length);
+
+			const text = txt1 + (txt2 === str ? '' : txt2 + str) + txt3 + (txt4 === str ? '' :  str + txt4) + txt5;
+			this.set_text(text, (str === txt2 ? -str.length : str.length), (str === txt4 ? -str.length : str.length));
+		},
+
+		set_header(lvl) {
+			const textarea = document.getElementById('source');
+			const start = textarea.selectionStart;
+			const end = textarea.selectionEnd;
+			const value = textarea.value
+			const line_start = value.substr(0, start).lastIndexOf('\n')+1;
+			const value2 = value.substr(line_start);
+
+			let hlvl = 0;
+			for (let i = 0; i < 7; i++) {
+				if (value2.charAt(i) === '#') hlvl++;
+				else if (value2.charAt(i) === ' ') {
+					hlvl++;
+					break;
+				} else {
+					hlvl = 0;
+					break;
+				}
+			}
+
+			let header = '';
+			for (let i = 0; i < lvl; i++) header = header + '#';
+			if (lvl !== 0) header = header + ' ';
+			
+			const txt1 = value.substr(0, line_start);
+			const txt2 = value.substr(line_start+hlvl);
+
+			this.set_text(txt1 + header + txt2, header.length-hlvl, header.length-hlvl);
 		}
 	}
 }
@@ -49,45 +100,8 @@ export default {
 
 <style scoped>
 	#toolbar {
-		height: 48px;
-		background-color: #4caf50;
 		margin-bottom: 10px;
-	}
-
-	.dropdown {
-		float: left;
-		width: 96px;
-		height: 32px;
-		margin: 8px;
-		margin-right: 0;
-		text-align: center;
-		font-size: 18px;
-		line-height: 1.8;
-		font-weight: bold;
 		background-color: #81c784;
-		color: #c8e6c9;
-		transition: all 0.3s;
-	}
-
-	.button {
-		float: left;
-		width: 32px;
-		height: 32px;
-		margin: 8px;
-		margin-right: 0;
-		text-align: center;
-		font-size: 24px;
-		background-color: #81c784;
-		color: #c8e6c9;
-		transition: all 0.3s;
-	}
-
-	.dropdown:hover, .button:hover {
-		background-color: #66bb6a;
-	}
-
-	.dropdown:active, .button:active {
-		background-color: #43a047;
 	}
 
 	textarea {
@@ -101,5 +115,6 @@ export default {
 
 	.markdown {
 		border-right: dotted 1px #81c784;
+		text-align: justify;
 	}
 </style>
